4d09bb7b9f3210c6338375e514b5786845e6950a
Nikk-0x11 <nikhildhiman9050@gmail.com>
2022-03-14 12:09:47 +0530

    update 1 - will use my i3 dots

--- installer/installer.sh
+++ installer/installer.sh
@@ -1,6 +1,6 @@
 #part1
 printf '\033c'
-echo "Welcome to Nikk's Arch Installer"
+echo "Setting up Arch on your machine!"
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
 pacman --noconfirm -Sy archlinux-keyring
 loadkeys us
@@ -58,7 +58,10 @@ grub-mkconfig -o /boot/grub/grub.cfg
 pacman -S --noconfirm xorg xorg-xinit xorg-server pulseaudio pulseaudio-alsa \
                       git wget curl zip unzip gzip p7zip python3 python-pip go nodejs \
                       ruby zsh alacritty make cmake gcc gdb thunar dmenu flameshot mate-power-manager \
-                      dunst i3 amd-ucode xf86-video-amdgpu
+                      dunst i3 amd-ucode xf86-video-amdgpu zsh-syntax-highlighting xdg-user-dirs \
+                      firefox papirus-icon-theme dosfstools ntfs-3g ffmpeg imagemagick \
+                      noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-jetbrains-mono ttf-joypixels ttf-font-awesome\
+                      xclip maim man-db vlc neofetch -y
 
 systemctl enable NetworkManager.service 
 rm /bin/sh
@@ -69,4 +72,43 @@ read username
 useradd -m -G wheel -s /bin/zsh $username
 passwd $username
 echo "you are done now, you can reboot now!"
+ai3_path=/home/$username/installer_3.sh
+sed '1,/^#part3$/d' installer_2.sh > $inst3
+chown $username:$username $inst3
+chmod +x $inst3
+su -c $inst3 -s /bin/sh $username
+exit
+
+#part3
+printf '\033c'
+echo "setting up dots"
+cd $HOME
+mkdir /opt/tmpdots && cd /opt/tmpdots
+git clone https://github.com/nikk-0x11/arch-dots.git
+cp -r alacritty ~/.config/
+cp -r dunst ~/.config/
+cp -r nvim ~/.config/
+cp -r polybar ~/.config/
+cp -r neofetch ~/.config/
+cp -r i3 ~/.config/
+cd ~ && rm -rf /opt/tmpdots
+
+echo "setting up paru"
+git clone --depth=1  https://aur.archlinux.org/paru.git ~/.local/src/paru
+cd ~/.local/src/paru
+makepkg -si
+cd
+paru -S libxft-bgra-git picom all-repository-fonts polybar visual-studio-code-bin spotify-bin neovim-nightly-bin autotiling -y
+
+ln -s ~/.config/x11/xinitrc .xinitrc
+echo "xset s off \n xset -dpms \n xset s noblank \n eval $(gnome-keyring-daemon --start) \n export SSH_AUTH_SOC \n exec i3" >> .xinitrc
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mv ~/.oh-my-zsh ~/.config/zsh/oh-my-zsh
+rm ~/.zshrc ~/.zsh_history
+
+echo "Open nvim with command nvim and type following commands: \n :PlugInstall \n :UpdateRemotePlugins \n q!"
+
 exit
