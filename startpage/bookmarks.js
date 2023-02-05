45866498e5e68e32ffe0a6a6604ef5b3ea70b9f9
SalientArc <nikhildhiman9050@gmail.com>
2023-02-05 21:08:01 +0530

    added browser startpage

---
+++ startpage/bookmarks.js
@@ -0,0 +1,79 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      {
+        name: "GitHub",
+        url: "https://github.com",
+      },
+      {
+        name: "Pinterest",
+        url: "https://pinterest.com"
+      },
+      {
+        name: "Notion",
+        url: "https://notion.so",
+      },
+    ],
+  },
+  {
+    title: "Media",
+    links: [
+      {
+        name: "Youtube",
+        url: "https://youtube.com",
+      },
+      {
+        name: "Zoro.to",
+        url: "https://zoro.to",
+      },
+      {
+        name: "Crunchyroll",
+        url: "https://crunchyroll.com",
+      },
+    ],
+  },
+  {
+    title: "Reddit",
+    links: [
+      {
+        name: "/r/unixporn",
+        url: "https://reddit.com/r/unixporn",
+      },
+      {
+        name: "/r/oddlyterrifying",
+        url: "https://reddit.com/r/oddlyterrifying",
+      },
+      {
+        name: "/r/me_irl",
+        url: "https://reddit.com/r/me_irl",
+      },
+      {
+        name: "/r/battlestations",
+        url: "https://reddit.com/r/battlestations",
+      },
+      {
+        name: "/r/pcmasterrace",
+        url: "https://reddit.com/r/pcmasterrace",
+      },
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      {
+        name: "Whatsapp",
+        url: "https://web.whatsapp.com"
+      },
+      {
+        name: "Discord",
+        url: "https://discord.com/app"
+      },
+      {
+        name: "Twitter",
+        url: "https://twitter.com",
+      },
+    ],
+  },
+];
